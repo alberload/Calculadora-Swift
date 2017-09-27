365e1e8581afcb17fba42b80f68ae91b947bad16
CETYS <cetys@cetyss-mac.local>
2017-09-27 13:12:33 +0200

    Vamos a intentarlo

--- Calculadora/ViewController.swift
+++ Calculadora/ViewController.swift
@@ -10,6 +10,48 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var pantalla: UILabel!
+    @IBOutlet weak var miniPantalla: UILabel!
+    
+    var elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero : Bool = false
+    var operando1 : Double = 0
+    var operando2 : Double = 0
+    var operacion : String = ""
+    
+    
+    @IBAction func enterPulsado() {
+        let numero : NSString = pantalla.text! as NSString
+        operando2 = numero.doubleValue
+        
+        let resultado = realizaOperacion()
+        pantalla.text = String(format: "%g", resultado)
+        miniPantalla.text = ""
+    }
+    
+    @IBAction func operacionPulsada(_ sender: UIButton) {
+        let numero : NSString = pantalla.text! as NSString
+        operando1 = numero.doubleValue
+        
+        //guardo la operacion pulsada
+        operacion = sender.currentTitle!
+        miniPantalla.text = pantalla.text
+        //pongo un 0 en la pantalla
+        pantalla.text = "0"
+        elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero = false
+    }
+    
+    @IBAction func numeroPulsado(_ sender: UIButton) {
+        let digito : String! = sender.currentTitle
+        
+        if elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero{
+            pantalla.text = pantalla.text! + digito
+        }
+        else {
+            pantalla.text = digito
+            elUsuarioEstaEnMedioDeLaEscrituraDeUnNumero = true
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +62,15 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func realizaOperacion() -> Double{
+        var resultado : Double = 0
+        
+        if operacion == "+"{
+            resultado = (operando1 + operando2)
+        }
+        
+        return resultado
+    }
 
 }
 
